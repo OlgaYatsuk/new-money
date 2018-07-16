775231954083d740d4821e4d56c963a130ba67be
olga <olgaa.yatsuk@gmail.com>
2018-07-16 15:39:31 +0300

    gh pages;

--- gulp/tasks/watch.js
+++
@@ -1,9 +0,0 @@
-const gulp   = require('gulp');
-// const config = require('../config');
-
-gulp.task('watch',
-    ['copy:watch',
-    'images:watch',
-    'webpack:watch',
-    'styles:watch'
-]);
